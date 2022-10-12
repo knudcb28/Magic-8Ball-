0e0af4e256faf305603061a9e3a8e07e38991b1f
Chris Knudsvig <92176945+knudcb28@users.noreply.github.com>
2022-10-11 23:54:15 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,39 @@
+let userName = 'Chris';
+
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+const userQuestion = 'Will I become rich?';
+console.log(userQuestion);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+switch(randomNumber) {
+  case 0: 
+    eightBall = 'It is certain';
+    break;
+  case 1: 
+    eightBall = 'It is decidely so';
+    break;
+  case 2: 
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3: 
+    eightBall = 'Cannot predict right now';
+    break;
+  case 4: 
+    eightBall = 'Do not count on it';
+    break;
+  case 5: 
+    eightBall = 'My sources say no';
+    break;
+  case 6: 
+    eightBall = 'Outlook not so good';
+    break;
+  case 7: 
+    eightBall = 'Signs point to yes';
+    break;
+}
+
+console.log (`The Magic 8Ball says: ${eightBall}.`)
